27e309ebc00f902eaecce16b0c39598d139680a8
Dennis Heibült <mail@dennisheibuelt.com>
2019-10-01 10:10:00 +0200

    feat: add commit linting
    * feat: add commit linting
    
    force adhering to the conventional commit guidelines by automatically
    lint commit messages for compliance. this is done on a commit hook
    via the commitlint and husky packages.
    
    * build(git-hooks): add external husky config
    
    - move config from `package.json` to external config `.huskyrc`
    
    * docs(contributing): adjust commit types
    
    - change list to comply with angular guidelines
    - add allowed types to contribution guidelines

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'release',
+                'style',
+                'test'
+            ]
+        ]
+    }
+};
